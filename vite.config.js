d8f6ecb90e07aadeac1278ef0fdf1c7e81e8de59
bhonemyatthingar2010-oss <bhonemyatthingar2010@gmail.com>
2026-08-23 19:11:46 +0630

    o
    o

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  // Required when hosting at https://<user>.github.io/stockpulse/
+  base: "/stockpulse/",
   plugins: [
     {
       name: "normalize-windows-paths",
